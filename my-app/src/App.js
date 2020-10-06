a3f54e16b7431351cfd33d22c97088bbc494510c
imfarooq24 <imfarooq24@gmail.com>
2020-10-06 20:37:21 +0500

    commit module 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,28 +23,56 @@ class App extends Component {
       ]
     })
   }
+  inputNameHandler = (event) =>{
+    this.setState({
+          person: [
+        {name: event.target.value,age:21},
+        {name: event.target.value,age:22},
+        {name: event.target.value,age:23},
+        {name: event.target.value ,age:24}
+      ]
+    })
+  }
+
 
   render() {
+    // const myStyle = {
+    //   backgroundColor:'white',
+    //   font:'inherit',
+    //   border:'1px solid blue',
+    //   padding:'8px',
+    //   cursor:'pointer'
+    // };
     return (
       <div id="root">
+        
         <div className="App">
           <h1>Hey im a react app</h1>
           <p>This is really working</p>
-          <p>This is really working</p>
-          <h1>Hey im a react app</h1>
-          <p>This is really working</p>
-          <p>This is really working</p>
-          <button onClick={this.changeNameHandler.bind(this,"BUbU")}>Change Names</button>
+          
+          <button 
+          // style={myStyle}
+          onClick={() => this.changeNameHandler("BUbU")}>
+            Change Names
+          </button>
+          
           <PersonComp name={this.state.person[0].name}
-           age={this.state.person[0].age}/>
+           age={this.state.person[0].age}
+           inputName={this.inputNameHandler }/>
+
           <PersonComp name={this.state.person[1].name}
            age={this.state.person[1].age}
-           click={this.changeNameHandler.bind(this,"BUbU ji")}/>
+           inputName={this.inputNameHandler }/>
+
           <PersonComp name={this.state.person[2].name}
+           inputName={this.inputNameHandler }
            age={this.state.person[2].age}/>
+
           <PersonComp name={this.state.person[3].name}
-           age={this.state.person[3].age}/>
+           age={this.state.person[3].age}
+           inputName={this.inputNameHandler }/>
         </div>
+
       </div>
     );
   }
--- my-app/src/PersonComp/PersonComp.js
+++ my-app/src/PersonComp/PersonComp.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import "./PersonComp.css";
 
 const PersonComp = (props) => {
     return (
-        <div>
+        <div className="PersonComp">
             <p onClick={props.click}>Im {props.name}</p>
             <p>And my age is {props.age}</p>
+            <input type="text" value={props.name} onChange={props.inputName}/>
         </div>
     )
 }
